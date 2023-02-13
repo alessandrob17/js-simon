5062c0c90816e5e0a2e697c6f3c6b04413340a99
alessandrob17 <a.baldipardi@gmail.com>
2023-02-13 21:54:19 +0100

    FEAT number

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-let randomNum = [];
-
+let numbers = [];
 for (let i = 0; i < 5; i++) {
-    randomNum[i] = Math.floor(Math.random() * 100);
+  numbers.push(Math.floor(Math.random() * 100 + 1));
 }
 
-alert(randomNum);
+alert(numbers);
 
+setTimeout(function() {
+  let userNumbers = [];
+  for (let i = 0; i < 5; i++) {
+    userNumbers.push(parseInt(prompt("Inserisci il numero " + (i + 1))));
+  }
+
+  let correctNumbers = [];
+  for (let i = 0; i < 5; i++) {
+    if (numbers.includes(userNumbers[i])) {
+      correctNumbers.push(userNumbers[i]);
+    }
+  }
+
+  console.log(`Hai indovinato ${correctNumbers.length} numeri: ${correctNumbers}`);
+}, 30000);
